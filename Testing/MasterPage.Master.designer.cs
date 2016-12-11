f461da0005be58909375f1edb927365157a6f355
Jabezyong <jabez.yong1215@gmail.com>
2016-12-11 18:51:42 +0800

    Added Registration and LoginView on MasterPage.

--- Testing/MasterPage.Master.designer.cs
+++ Testing/MasterPage.Master.designer.cs
@@ -76,6 +76,15 @@ namespace Testing {
         protected global::System.Web.UI.WebControls.HyperLink HyperLink4;
         
         /// <summary>
+        /// LoginView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginView LoginView1;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
